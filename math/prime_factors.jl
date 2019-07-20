afa2f716a2140f2260e496187dfe8617e98c011e
Parth Paradkar <parthparadkar3@gmail.com>
2019-07-21 00:52:25 +0530

    Prime factors added

---
+++ math/prime_factors.jl
@@ -0,0 +1,21 @@
+function prime_factors(num::Int)::Array
+    if(num <= 0)
+        return error("Enter a positive integer!")
+    end
+    n = num
+    i :: Int = 2
+    factors= []
+    while i*i <= n
+        if (n % i != 0)
+            i += 1
+        else
+            n = n / i
+            push!(factors, i)
+        end
+    end
+    if (n > 1)
+        push!(factors, n)
+    end
+end
+
+println(prime_factors(288))
